478c0be1a2724840823afb60dd8be37cc55ed192
Xabi <xabi.aycaguer@gmail.com>
2025-09-22 15:04:15 +0200

    :sparkles: update table modew

--- home/.config/nvim/lua/plugins/table-mode.lua
+++ home/.config/nvim/lua/plugins/table-mode.lua
@@ -4,7 +4,12 @@ return {
     init = function()
       vim.g.table_mode_corner = "|"
       vim.g.table_mode_header_fillchar = "-"
-      vim.g.table_mode_always_active = 1
+      vim.g.table_mode_always_active = 0
     end,
+    keys = {
+      { "<leader>tm", "<cmd>TableModeToggle<cr>", desc = "Toggle table mode" },
+      { "<leader>tr", "<cmd>TableModeRealign<cr>", desc = "Realign table" },
+      { "<leader>tt", "<cmd>'<,'>Tableize<cr>", mode = "v", desc = "Tableize selection" },
+    },
   },
 }
